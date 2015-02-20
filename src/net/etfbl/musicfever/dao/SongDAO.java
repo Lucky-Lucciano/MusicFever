2ac517d21334a79af374b34968e924722f85e219
Ays <luka_t@hotmail.de>
2015-02-20 21:17:30 +0100

    init two

--- src/net/etfbl/musicfever/dao/SongDAO.java
+++ src/net/etfbl/musicfever/dao/SongDAO.java
@@ -1,36 +1,149 @@
 package net.etfbl.musicfever.dao;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import net.etfbl.musicfever.dto.Song;
 
 public class SongDAO {
-	private static final String SQL_LOGIN = "SELECT * FROM user WHERE username = ? and password = md5(?) limit 1";
-	private static final String SQL_ADD_USER = "INSERT INTO users(username, password, name, surname, email, usergroup, JMBG, registration_date, active, approved, superuser, image) values(?, md5(?), ?, ?, ?, ?, ?, NOW(), ?, ?, ?, ?)";
-	private static final String SQL_APPROVE_USER = "UPDATE user SET approved = 1 WHERE id=?";
-	private static final String SQL_ALL_USERS = "SELECT * FROM user";
-	private static final String SQL_USERNAME_AVAILABLE = "SELECT count(*) FROM user WHERE username=?";
+	private static final String SQL_SONG_DETAILS = "SELECT * FROM song WHERE id=? limit 1";
+	private static final String SQL_ADD_SONG = "INSERT INTO song(name, artist, release_date, duration, file_type, lyrics, location, active, registration_date) values(?, ?, ?, ?, ?, ?, ?, ?, NOW())";
+	private static final String SQL_UPDATE_SONG = "UPDATE song SET name=?, artist=?, realease_date=?, duration=?, file_type=?, lyrics=?, location=? WHERE id=?";
+	private static final String SQL_DELETE_SONG = "UPDATE song SET active=0 WHERE id=?";
+	private static final String SQL_ALL_USERS = "SELECT * FROM user WHERE usergroup=0 AND active=1";
 	
-	// Ajaxom se moze provjeriti je l dostupno - ako ne bude vremena izbaciti
-	public static boolean usernameAvailable(String name) {
+	public static boolean deleteSong(Song song) {
 		Connection connection = null;
-		boolean rezultat = false;
-		Object values[] = {name};
+		Object values[] = {song.getId()};
 		
 		try {
 			connection = ConnectionPool.getConnectionPool().checkOut();
-			PreparedStatement pstmt = DAOUtil.prepareStatement(connection, SQL_USERNAME_AVAILABLE, false, values);
-			ResultSet rs = pstmt.executeQuery();
-			if(rs.next())
-				rezultat = (rs.getInt(1) == 0);
-			else
-				rezultat = false;
-			rs.close();
+			PreparedStatement pstmt = DAOUtil.prepareStatement(connection, SQL_DELETE_SONG, false, values);
+			pstmt.executeUpdate();
 			pstmt.close();
-			return rezultat;
+			return true;
 		} catch(Exception ex) {
+			System.out.println("Song delete exception");
 			ex.printStackTrace(System.err);
 			return false;
+		} finally {
+			ConnectionPool.getConnectionPool().checkIn(connection);
+		}
+	}
+	
+	public static boolean updateSong(Song song) {
+		Connection connection = null;
+		boolean retVal = false;
+		Object[] values = {song.getName(), song.getArtist(), song.getReleaseDate(), song.getDuration(), song.getFileType(), song.getLyrics(), song.getLocation(), song.getId()};
+		// DODATI I IZMJENE NA GENRE!!!!!!!
+		try {
+			connection = ConnectionPool.getConnectionPool().checkOut();
+			PreparedStatement ps = DAOUtil.prepareStatement(connection, SQL_UPDATE_SONG, false, values);
+			int rez = ps.executeUpdate();
+			if (rez == 0) {
+				retVal = false;
+				System.out.println("Couldn't update song");
+			} else {
+				retVal = true;
+				System.out.println("Song updated");
+			}
+			ps.close();
+			return retVal;
+		} catch (SQLException e) {
+			System.out.println("Update song exception " + e);
+			return retVal;
+		} finally {
+			ConnectionPool.getConnectionPool().checkIn(connection);
+		}
+	}
+	
+	public static Song addUser(Song song) {
+		Connection connection = null;
+		ResultSet generatedKeys = null;
+
+		Object[] values = {song.getName(), song.getArtist(), song.getReleaseDate(), song.getDuration(), song.getFileType(), song.getLyrics(), song.getLocation(), 1};
+		// Definitvno provjeriti upit u bazi
+		// UBACITI GENRE I OSTALO
+		try {
+			connection = ConnectionPool.getConnectionPool().checkOut();
+			PreparedStatement ps = DAOUtil.prepareStatement(connection, SQL_ADD_SONG, true, values);
+			int rez = ps.executeUpdate();
+			if (rez == 0) {
+				song = null;
+				System.out.println("Couldn't add song");
+			} else {
+				generatedKeys = ps.getGeneratedKeys();
+				System.out.println("Song added");
+				//Povratni info za ID
+				if (generatedKeys.next()) {
+					song.setId(generatedKeys.getInt(1));
+				}
+			}
+			ps.close();
+		} catch (SQLException e) {
+			System.out.println("Add song exception " + e);
+			return null;
+		} finally {
+			ConnectionPool.getConnectionPool().checkIn(connection);
+		}
+		return song;
+	}
+	
+	// DA PRIHVATA VISE ZANROVA I VRATI NA OSNOVU PREGLEDA???
+	public static ArrayList<Song> getSongsByGenre() {
+		Connection connection = null;
+		ArrayList<Song> rez = new ArrayList<Song>();
+		Object values[] = {};
+		
+		try {
+			connection = ConnectionPool.getConnectionPool().checkOut();
+			PreparedStatement pstmt = DAOUtil.prepareStatement(connection, SQL_ALL_USERS, false, values);
+			ResultSet rs = pstmt.executeQuery();
+			while(rs.next()) {
+				/*rez.add(new User(rs.getInt(1), rs.getInt(7), rs.getInt(10) == 1, rs.getInt(11) == 1, rs.getInt(12) == 1, rs.getString(2),
+									rs.getString(3), rs.getDate(9), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(12), rs.getString(13)));*/
+			}			
+			rs.close();
+			pstmt.close();
+			return rez;
+		} catch(Exception ex) {
+			ex.printStackTrace(System.err);
+			return rez;
+		} finally {
+			ConnectionPool.getConnectionPool().checkIn(connection);			
+		}
+	}
+	
+	public static Song getSongDetails(Song song) {		
+		Connection connection = null;
+		Song songRet = null;
+		Object values[] = {song.getId()};
+		
+		try {
+			connection = ConnectionPool.getConnectionPool().checkOut();
+			PreparedStatement pstmt = DAOUtil.prepareStatement(connection, SQL_SONG_DETAILS, false, values);
+			ResultSet rs = pstmt.executeQuery();
+			if (rs.next()){
+				/*int id, String artist, String name, boolean active, int fileType,
+				int releaseDate, int duration, Date registrationDate,
+				String location, String lyrics*/
+				songRet = new Song(rs.getInt(1), rs.getString(7), rs.getString(10), rs.getInt(11) == 1, rs.getInt(12), rs.getInt(2),
+									rs.getInt(3), rs.getDate(9), rs.getString(4), rs.getString(5));
+			}
+			
+			// Get AllGenre 
+			rs.close();
+			pstmt.close();
+			
+			return songRet;
+		} catch (SQLException e) {
+			System.out.println("Song details exception " + e);
+			e.printStackTrace();
+			return songRet;
 		} finally {
 			ConnectionPool.getConnectionPool().checkIn(connection);
 		}
